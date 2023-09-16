d2c8563a2f4f34e491875d170e432748125d653e
Gilmer Contreras <adgdiux@gmail.com>
2023-09-15 20:54:58 -0500

    Arreglos

--- index.js
+++ index.js
@@ -6,47 +6,55 @@ const fetch = (...args) =>
 
 const server = net.createServer();
 
-server.on('connection', (socket) => {
-    socket.on('data',async (data) => {
+try {
 
-        let datos= data.toString().split('_');
-
-        if (datos[1]) {
-
-            // let date = new Date().getTime();
-
-            let body = {
-                "code": datos[0],
-                "temperatura": datos[1],
-                "fecha": new Date()
-            }
-
-            console.log(body);
-
-            const response = await fetch(process.env.URL, {
-                method: 'post',
-                body: JSON.stringify(body),
-                headers: {
-                    'Content-Type': 'application/json',
+    server.on('connection', (socket) => {
+        socket.on('data',async (data) => {
+    
+            let datos= data.toString().split('_');
+    
+            if (datos[1]) {
+    
+                // let date = new Date().getTime();
+    
+                let body = {
+                    "code": datos[0],
+                    "temperatura": datos[1],
+                    "fecha": new Date()
                 }
-            });    
-            
-            let resp = await response.json();
-
-            if (!resp.ok) {
-                console.log(`${resp.msg} codigo: ${body.code} temperatura: ${body.temperatura}`);
-            }else{
-                console.log(resp.msg);
-            }
-
-        }       
-
-    });
-
-    socket.on('error', (err) =>{
-        console.log(err);
+    
+                console.log(body);
+    
+                const response = await fetch(process.env.URL, {
+                    method: 'post',
+                    body: JSON.stringify(body),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                });    
+                
+                let resp = await response.json();
+    
+                if (!resp.ok) {
+                    console.log(`${resp.msg} codigo: ${body.code} temperatura: ${body.temperatura}`);
+                }else{
+                    console.log(resp.msg);
+                }
+    
+            }       
+    
+        });
+    
+        socket.on('error', (err) =>{
+            console.log(err);
+        })
     })
-})
+    
+} catch (error) {
+    console.log(err);
+}
+
+
 
 server.listen(process.env.PORT, ()=>{
     console.log('Servidor corriendo en el puerto ', process.env.PORT);
